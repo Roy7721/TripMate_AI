8e8032f67fe65fedd636ca4ffc59fc21748fa4bc
Roy7721 <s2010724129@ru.ac.bd>
2026-09-19 04:34:25 +0600

    flight tool file added

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-from tools.tavily_tool import tavily_search
+# from tools.tavily_tool import tavily_search
 
-result = tavily_search("capital of bangladesh")
+# result = tavily_search("capital of bangladesh")
 
-print(result)
+# print(result)
+
+from tools.flight_tool import search_flights
+
+print(search_flights("Plan a 7 days Japan trip from Bangladesh"))
+print("\n" + "=" * 80 + "\n")
+print(search_flights("all country flight info"))
